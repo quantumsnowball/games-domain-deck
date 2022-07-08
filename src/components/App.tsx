03b97d9f1172587ce25fc0e9610e49ade967844c
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-09 04:55:00 +0800

    reorganize folder structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../style/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           hosting at: games.domain/deck
         </p>
